cc9708f6a6d4f3a2163bfcbaa4837cf5dadd424b
vernaloqui <vernadetteloquinario@gmail.com>
2022-12-05 19:00:20 +0800

    commit

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -11,9 +11,9 @@ function Category(){
             <h1>Category</h1>
             <div >
                 <div className="col mb-3" style={{display:'inline-flex'}}>
-                    <Card img={cat1} title={'Carpenter'} text={'sample text'} />
-                    <Card img={cat2} title={'Plumber'} text={'sample text'} />
-                    <Card img={cat3} title={'Painter'} text={'sample text'} />
+                    <Card img={cat1} title={'Carpenter'} text={''} />
+                    <Card img={cat2} title={'Plumber'} text={''} />
+                    <Card img={cat3} title={'Painter'} text={''} />
                 </div>
             </div><br/><br/>
             <div className="box">
@@ -22,9 +22,9 @@ function Category(){
             <br/><br/>
             <div >
                 <div className="col mb-3" style={{display:'inline-flex'}}>
-                    <Card img={cat4} title={'House Cleaner'} text={'sample text'} />
-                    <Card img={cat5} title={'Electrician'} text={'sample text'} />
-                    <Card img={cat6} title={'Welder'} text={'sample text'} />
+                    <Card img={cat4} title={'House Cleaner'} text={''} />
+                    <Card img={cat5} title={'Electrician'} text={''} />
+                    <Card img={cat6} title={'Welder'} text={''} />
                 </div>
             </div>
         </div>
